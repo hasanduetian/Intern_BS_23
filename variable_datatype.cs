3a30d14e92237ca8e7c99b1dfa6300c62f7c975e
Mahamudul Hasan <hasanrpi024@gmail.com>
2026-05-19 17:07:09 +0600

    file added

---
+++ variable_datatype.cs
@@ -0,0 +1,30 @@
+using System;
+namespace hasan
+{
+    class Program
+    {
+        public static MatchCasing()
+        {
+            int num = 11;
+            double bigName = num;
+            Console.WriteLine(bigName);
+
+            // explicit conversion -> possible data loss
+            double c = 10.13;
+            int d = (int)c;   // casting
+            Console.WriteLine(d);
+
+            // convert string
+            string tmp="123";
+            int res=Convert.ToInt32(tmp);
+            Console.WriteLine(res);
+            Console.WriteLine(res.GetType());
+
+            // decimal datatype
+            decimal a=10.54774;// high precision
+            Console.WriteLine(a);
+        }
+    }
+}  
+  
+ 
