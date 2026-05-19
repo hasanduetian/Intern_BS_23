84f8ef5323d10592ace4ad3ae618c0bc7e9c26e2
Mahamudul Hasan <hasanrpi024@gmail.com>
2026-05-19 17:48:05 +0600

    loop file create

---
+++ loop.cs
@@ -0,0 +1,24 @@
+using System;
+namespace hasan
+{
+    class Program
+    {
+        public static Main()
+        {
+            // For loop
+            for (int i = 0; i < 5; i++) {
+            Console.WriteLine(i);
+            }
+
+            // While Loop
+            int i = 0;
+            while (i < 5)
+            {
+            Console.WriteLine(i);
+            i++;
+            }
+        }
+    }
+}  
+  
+ 
