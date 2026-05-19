3a208044b5b24557eacb2f23c90088ebf07741a9
Mahamudul Hasan <hasanrpi024@gmail.com>
2026-05-19 18:02:05 +0600

    added

---
+++ function.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    // Function
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        int result = Add(5, 3);
+
+        Console.WriteLine("Sum = " + result);
+    }
+}
